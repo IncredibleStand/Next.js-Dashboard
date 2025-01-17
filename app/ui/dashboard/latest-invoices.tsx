486c4187643c14133403c56729a1b14f39f92be0
IncredibleStand <incrediblestand@gmail.com>
2025-01-17 12:56:32 +0100

    Update latest-invoices.tsx

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,6 +3,15 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 
+type LatestInvoice = {
+  id: string;
+  name: string;
+  email: string;
+  amount: string;
+  image_url: string;
+};
+
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
